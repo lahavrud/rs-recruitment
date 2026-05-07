4c44bf09fbf212393371757471afc8101e096b09
Lahav Rudik <lahavrud@gmail.com>
2026-05-07 23:13:52 +0300

    fix: ruff import order in migration file

--- alembic/versions/6484c273cd57_add_composite_index_application_job_.py
+++ alembic/versions/6484c273cd57_add_composite_index_application_job_.py
@@ -5,17 +5,16 @@ Revises: 175fb94581a1
 Create Date: 2026-05-07 23:05:07.375643
 
 """
-from typing import Sequence, Union
+
+from collections.abc import Sequence
 
 from alembic import op
-import sqlalchemy as sa
-
 
 # revision identifiers, used by Alembic.
-revision: str = '6484c273cd57'
-down_revision: Union[str, Sequence[str], None] = '175fb94581a1'
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+revision: str = "6484c273cd57"
+down_revision: str | Sequence[str] | None = "175fb94581a1"
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
